97e2eb9b79af07ef616f5fcd1de660ac16e09775
Ramón Huidobro <hola@ramonh.dev>
2020-09-16 14:47:28 +0200

    Update dates

--- src/mock/data.js
+++ src/mock/data.js
@@ -48,8 +48,8 @@ export const detailsData = [
     img: 'chums.jpg',
     title: 'When do you meet?',
     info: 'Every two weeks (from 18:30 to 21:00):',
-    info2: 'Wednesday, 19th of August',
-    info3: 'Wednesday, 2nd of September',
+    info2: 'Wednesday, 16th of September',
+    info3: 'Wednesday, 30th of September',
     url: '',
     repo: '', // if no repo, the button will not show up
   },
