a6e0ca19e69f2817919c92f618bdc9abbef6a57f
Ramón Huidobro <hola@ramonh.dev>
2021-01-20 15:41:52 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-195bab15239a81237e21.js"
+    "url": "webpack-runtime-a852be9426bc7f8191a8.js"
   },
   {
     "url": "commons-17850e880a30a9454e8a.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "82a25f2821f595785773bf738fb4ab8c"
+    "revision": "f3b94c4b25cfbcccbae5bab5344a7be3"
   },
   {
     "url": "manifest.webmanifest",
